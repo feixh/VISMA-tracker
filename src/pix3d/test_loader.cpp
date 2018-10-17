7427fbe8c81ecebaf6b05fca6fcf6889d9ed083f
feixh <hzhsfxh@gmail.com>
2018-10-16 19:54:54 -0700

    not work yet

--- src/pix3d/test_loader.cpp
+++ src/pix3d/test_loader.cpp
@@ -13,7 +13,9 @@ int main() {
     cv::imshow("mask", packet._mask);
 
     std::cout << "object pose=\n" << packet._go.matrix3x4() << std::endl;
-    std::cout << "camera pose=" << packet._gc.matrix3x4() << std::endl;
+    std::cout << "object pose inverse=\n" << packet._go.inverse().matrix3x4() << std::endl;
+    std::cout << "camera pose=\n" << packet._gc.matrix3x4() << std::endl;
+    std::cout << "camera pose inverse=" << packet._gc.inverse().matrix3x4() << std::endl;
     std::cout << "focal=" << packet._focal_length << std::endl;
     std::cout << "bbox=" << packet._bbox.transpose() << std::endl;
     std::cout << "shape=" << packet._shape.transpose() << std::endl;
@@ -21,7 +23,10 @@ int main() {
     std::cout << packet._V.colwise().mean() << std::endl;
     Eigen::MatrixXf V(packet._V.rows(), packet._V.cols());
     for (int i = 0; i < V.rows(); ++i) {
-        V.row(i) = packet._go * packet._V.row(i);
+        // V.row(i) = packet._go * packet._V.row(i);
+        feh::Vec3 X(packet._V(i, 0), packet._V(i, 1), packet._V(i, 2));
+        X = packet._go * X;
+        V.row(i) = X.transpose();
     }
 
     // std::cout << V << std::endl;
@@ -51,29 +56,9 @@ int main() {
     mask.setTo(0);
     Eigen::Matrix4f pose = Eigen::Matrix4f::Identity();
     pose.block<3, 4>(0, 0) = packet._gc.inverse().matrix3x4();
-    // pose(2, 3) = 5.0;
+    // pose(2, 3) = -2.0;
     engine->RenderMask(pose, mask);
     cv::imshow("rendered mask", mask);
     cv::waitKey(0);
     engine.reset();
-
-
-    // // sanity check -- render the model at the given pose and compare to RGB image
-    // auto engine = std::make_shared<feh::Renderer>(packet._shape[0], packet._shape[1]);
-    // engine->SetCamera(znear, zfar, 
-    //         packet._focal_length, packet._focal_length, 
-    //         packet._shape[1] >> 1, packet._shape[0] >> 1);
-    // // Eigen::MatrixXf V = packet._V * packet._go.so3().matrix();
-    // // V += packet._go.translation().transpose();
-    // engine->SetMesh((float*)packet._V.data(), packet._V.rows(), (int*)packet._F.data(), packet._F.rows());
-    // // render mask
-    // cv::Mat mask(packet._shape[0], packet._shape[1], CV_8UC1);
-    // mask.setTo(0);
-    // Eigen::Matrix4f pose = Eigen::Matrix4f::Identity();
-    // // pose.block<3, 4>(0, 0) = packet._gc.matrix3x4();
-    // pose(1, 3) = 5.0;
-    // engine->RenderMask(pose, mask);
-    // cv::imshow("rendered mask", mask);
-    // cv::waitKey(0);
-    // engine.reset();
 }
